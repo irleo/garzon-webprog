f59fbc399b45d245650ac0124726627988cb0064
unknown <meyjajasin@gmail.com>
2026-03-13 15:36:21 +0800

    lab-act1

---
+++ garzon-client/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My React App</h1>
+
+        <p>
+          Name: Leomar M. Garzon <br />
+          Section: INF235 <br />
+          <p>Github: irleo</p>
+          <p>
+            Repository:{" "}
+            <a href="https://github.com/irleo/garzon-webprog" target="_blank">
+              https://github.com/irleo/garzon-webprog
+            </a>
+          </p>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
